8a3baddebb854180571012f3883e2c46335e2b6b
Doriane.D <68782490+Satyryasis@users.noreply.github.com>
2020-11-07 00:28:45 +0100

    main
    ATTENTION : Le programme n'est pas encore abouti. Il ne retourne pas systématiquement les bonnes valeurs concernant les solutions x0, x1 et x2. De plus, le discriminant "delta" peut retourner une valeur incorrecte lorsque les arguments font que le résultat est flottant.

---
+++ Source.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char* argv[])
+{
+	int val_A = 0;
+	int val_B = 0;
+	int val_C = 0;
+	int delta = 0;
+	int solx0 = 0;
+	int solx1 = 0;
+	int solx2 = 0;
+	char sup = '>';
+	char inf = '<';
+	char eq = '=';
+
+	printf("=================\n");
+	printf("CALCUL POLYNOMIAL\n");
+	printf("=================\n");
+	puts("");
+	printf("La forme d'un polynome correspondant a : ax^2 + bx + c, rentrez la valeur de  A, B et C.\n\n");
+	printf("Valeur de A : ");
+	scanf_s("%d", &val_A);
+	printf("Valeur de B : ");
+	scanf_s("%d", &val_B);
+	printf("Valeur de C : ");
+	scanf_s("%d", &val_C);
+	system("cls");
+	printf("=================\n");
+	printf("CALCUL POLYNOMIAL\n");
+	printf("=================\n\n");
+	printf("Format du polynome : %dx^2 + %dx + %d\n", val_A, val_B, val_C);
+	delta = pow(val_B, 2)-4*val_A*val_C;
+	printf("Le discriminant vaut %d\n", delta);
+	if (delta > 0)
+	{
+		printf("Delta %c 0\n", sup);
+		solx1 = (-val_B - sqrt(delta)) / 2 * val_A;
+		solx2 = (-val_B + sqrt(delta)) / 2 * val_A;
+		printf("Le polynome admet deux solutions : x1 = %d et x2 = %d\n\n\n", solx1, solx2);
+	}
+	else if (delta < 0)
+	{
+		printf("Delta %c 0\n", inf);
+		solx0 = -val_B / 2 * val_A;
+		printf("Le polynome admet une solution : x0 = %d\n\n\n", solx0);
+	}
+	else if (delta == 0)
+	{
+		printf("Delta %c 0", eq);
+		printf("Le polynome n'a pas de solutions dans R\a\n\n\n");
+	}
+	return(0);
+}
